8e4e0a868ec561e50ee9c16d2cf3f57c4f9f2f23
Lean Connector <dream.planner@leanconnector.net>
2020-12-21 08:27:39 +0900

    Update 20201221

--- app_rev2.js
+++ app_rev2.js
@@ -9,44 +9,71 @@ async function fyfy() {
 
   let url = document.URL;
   let title, desc = '';
-  let author, date, infos;
+  let author, date, infos, coverUrl;
 
-  if(url.startsWith('https://learning.oreilly.com/library/view/')) {
+  if(url.indexOf('oreilly.com/library/view/') > -1) {
     title  = document.querySelector('h1').innerText;
-    author = document.querySelector('div.author').innerText;
-    desc = document.querySelector('div.issued').innerText + '\n' + document.querySelector('div.isbn').innerText;
-    desc += '\n\n' + turndownService.turndown(document.querySelector('div.description div').innerHTML);
+    const authorEl = document.querySelector('div.author') || document.querySelector('div.t-authors');
+    const dateEl = document.querySelector('div.issued') || document.querySelector('div.t-release-date');
+    const isbnEl = document.querySelector('div.isbn') || document.querySelector('div.t-isbn');
+    const descEl = document.querySelector('div.description>span') || document.querySelector('div.title-description>div.content>span');
 
-    desc = "[오라일리 사파리](" + url + ") \n" + author + "\n\n" + desc;
+    let isbn = url.replace(/\/$/, '').split('/').pop();
+    coverUrl = 'https://learning.oreilly.com/library/cover/'+isbn+'/250w.jpg';
+    desc = '[오라일리 사파리](' + url + ')\n[cover link](' + coverUrl + ')\n'
+    + authorEl.innerText + '\n\n' + dateEl.innerText + '\n' + isbnEl.innerText + '\n\n'
+    + turndownService.turndown(descEl.innerHTML);
   }
-  else if(url.indexOf('pragprog.com/book/') > -1) {
+  else if(url.indexOf('pragprog.com/titles/') > -1) {
     title = document.getElementsByTagName('h1')[0].innerText;
-    author = document.querySelector('span[itemprop=author]').innerText;
-    desc = document.querySelector('article[itemprop=description]').innerText;
+    author = document.querySelector('h2.author').innerText;
+    desc = document.querySelectorAll('section.book-main')
 
-    desc = '[프래그매틱 링크](' + url + ') \nby ' + author + '\n\n';
-    desc += document.querySelector('div[class$=book-details]').innerText + '\n' + 
-            document.querySelector('article[itemprop=description]').innerText + '\n\n###목차\n\n';
-    infos = Array.from(document.querySelectorAll('article[id=toc]>ul>li'));
-    infos.forEach((item) => { desc += '- ' + item.innerText.split('\n')[0] + '\n' });
+    infos = [...document.querySelector('section.book-main').children];
+    let i = 0, started = null;
+    for(; i < infos.length; i++) {
+      if(infos[i].tagName == 'P')
+        if(!started) started = i;
+        else continue;
+      else if(started) break;
+    }
+    if(started)
+      desc = infos.slice(started, i).map(el => el.innerHTML).join('\n\n').trim();
+
+    coverUrl = document.querySelector('div.book-about-cover>a');
+    if(coverUrl) coverUrl = coverUrl.href;
+    
+    desc = '[프래그매틱 링크](' + url + ')\n[cover link](' + coverUrl + ')\n' + author + '\n\n' + turndownService.turndown(desc);
+
+    infos = [...document.querySelectorAll('section.book-main>ul')].slice(-1)[0];
+    if(infos) {
+      infos = [...infos.querySelectorAll('li')].filter(el => el.parentNode == infos).map(el => '- '+(el.firstChild.textContent || el.innerText).trim()).join('\n');
+    }
+
+    if(infos.length > 0)
+      desc += '\n\n###목차\n' + infos;
   }
   else if(url.indexOf('.packtpub.') > -1) {
     title = document.getElementsByTagName('h1')[0].innerText;
-    author = document.getElementsByClassName('book-top-block-info-authors left')[0].innerText.trim().split('\n')[0];
-    date = document.getElementsByClassName('book-top-block-info-authors left')[0].innerText.trim().split('\n')[1];
+    author = document.querySelector('span.product-info__author').innerText;
 
-    desc = "[팩트 링크](" + url + ") \nby " + author + "\n\n\n";
-    desc += document.getElementsByClassName('book-info-details onlyDesktop')[0].innerText + '\n' + date + '\n\n\n';
-    desc += document.getElementsByClassName('book-info-bottom-indetail book-page-content float-right')[0].innerText;
+    coverUrl = document.querySelector('div.image>div>img.product-image');
+    if(coverUrl) coverUrl = coverUrl.src;
+
+    desc = '[팩트 링크](' + url + ') \n[cover link](' + coverUrl + ')\n' + author + '\n\n';
+    desc += document.querySelector('dl.overview__datalist').textContent.trim().replace(':\n', '\n').split('\n\n\n').map(t => '- '+t.replace('\n', ': ')).join('\n') + '\n\n';;
+    desc += document.querySelector('div.overview__body').innerText;
   }
   else if(url.indexOf('.manning.') > -1) {
     title = document.head.querySelector("[name=application-name]").content;
     author = document.getElementsByClassName('product-authorship')[0].innerText.trim().split('\n')[0];
 
-    desc = "[매닝 링크](" + url + ") \nby " + author + "\n\n";
-    desc += [...document.getElementsByClassName('product-info')[0].querySelectorAll('li')].map(el => '- ' + el.innerText.trim()).join('\n');
-
-    desc += '\n\n' + turndownService.turndown(document.querySelector('div.description-body').innerHTML.trim());
+    coverUrl = document.querySelector('div#coverColumn>a>div');
+    if(coverUrl && coverUrl.style)
+      coverUrl = coverUrl.style.backgroundImage.replace(/^url\("/, '').replace(/"\)$/, '');
+    desc = '[매닝 링크](' + url + ') \n[cover link](' + coverUrl + ')\nby ' + author + '\n\n';
+    desc += [...document.getElementsByClassName('product-info')[0].querySelectorAll('li')].map(el => '- ' + el.innerText.trim()).join('\n') + '\n\n'
+    desc += turndownService.turndown(document.querySelector('div.description-body').innerHTML.trim());
 
     desc += '\n\n###' + document.querySelector('div.header').childNodes[0].textContent.trim();
     desc += '\n\n' + [...document.querySelector('div.toc').children].map(el => (el.querySelector('h2') || el).textContent).join('\n');
@@ -68,10 +95,10 @@ async function fyfy() {
     if(asin.indexOf('?') > -1) asin = asin.slice(0, asin.indexOf('?'));
     url = protocol + '//' + host + '/dp/' + asin;
 
-    title = document.getElementById('title').innerText;
+    title = document.getElementById('productTitle').innerText.trim() + ' / ' + document.getElementById('productSubtitle').innerText.trim();
     author = document.getElementById('bylineInfo');
     if(author === null) author = '';
-    else author = author.innerText;
+    else author = author.innerText.replace(/^기준/, 'by');  //한글(아마존 UI);
 
     //table of contents (packt only)
     let descBody = document.getElementById('bookDescription_feature_div');
@@ -80,13 +107,20 @@ async function fyfy() {
       .split('bookDescEncodedData = "')[1].split('",\n')[0]
       .split('Table%20of%20Contents%3C%2Fh4%3E')[1];
     }
+    else
+      descBody = null;
 
-    desc = document.querySelector('div#detailBullets_feature_div').innerText.replace(/\n\#/g, "\n\n- \\#");
+    desc = document.querySelector('div#detailBullets_feature_div').innerText
+    .replace(/\n\#/g, "\n\n- \\#")   //영문
+    .replace(/\n\에서 #/g, "\n\n- \\#");  //한글(아마존 UI)
     let subIdx = desc.lastIndexOf('- \\#');
     let sub = desc.slice(subIdx).replace("\n", "\n\n");
     desc = desc.slice(0, subIdx) + sub;
 
-    desc = '[아마존 링크](' + url + ') \n' + author + '\n\n' + desc;
+    coverUrl = document.querySelector('div#booksImageBlock_feature_div img#imgBlkFront, div#imageBlockNew_feature_div img#ebooksImgBlkFront');
+    if(coverUrl) coverUrl = coverUrl.src;
+
+    desc = '[아마존 링크](' + url + ')\n[cover link](' + coverUrl + ')\n' + author + '\n\n' + desc;
     if(descBody)
       desc += '\n\n----\n###목차\n' + turndownService.turndown(decodeURIComponent(descBody));
 
